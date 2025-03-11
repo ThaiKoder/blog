57d7507133d242ee9bff23de009de2ab3b84656c
ThaiKoder <thaikoder@gmail.com>
2025-03-11 07:37:16 +0100

    ✨ feat(layout): integrate microsoft clarity for user insights   - add microsoft clarity to track user behavior   - include script for clarity initialization with project id 📦 build(deps): add @microsoft/clarity package
    - install @microsoft/clarity package for user behavior analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,18 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script';
 
-// Retrieving Google Tag Manager ID from environment variables
+// Retrieving environments variables
 const GOOGLE_TAG_MANAGER_ID = process.env.GOOGLE_TAG_MANAGER_ID as string;
+const CLARITY_PROJECT_ID = process.env.CLARITY_PROJECT_ID as string;
 
 if (!GOOGLE_TAG_MANAGER_ID) {
   throw new Error('GOOGLE_TAG_MANAGER_ID is not defined in environment variables');
+}
+
+if (!CLARITY_PROJECT_ID) {
+  throw new Error('CLARITY_PROJECT_ID is not defined in environment variables');
 }
 
 // Configuration of Geist Sans font for general text
@@ -34,6 +40,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     // Setting the site language to French
     <html lang="fr">
@@ -45,6 +52,17 @@ export default function RootLayout({
 
         {/* Google Analytics integration */}
         <GoogleAnalytics gaId={GOOGLE_TAG_MANAGER_ID} />
+
+        {/* Microsoft Clarity integration */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${CLARITY_PROJECT_ID}");
+          `}
+        </Script>
       </body>
     </html>
   );
